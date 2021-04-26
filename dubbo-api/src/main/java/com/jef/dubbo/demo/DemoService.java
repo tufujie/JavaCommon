03ac29538b51b96f65f3590d9e99ed61c3cb5e51
jef.tu@foxmail.com <loveblog@520>
2021-04-26 20:51:43 +0800

    补充

--- dubbo-api/src/main/java/com/jef/dubbo/demo/DemoService.java
+++ dubbo-api/src/main/java/com/jef/dubbo/demo/DemoService.java
@@ -3,7 +3,9 @@ package com.jef.dubbo.demo;
 import java.util.List;
 
 /**
- * Created by wy on 2017/4/13.
+ * dubbo接口
+ * @author Jef
+ * @date 2021/4/26
  */
 public interface DemoService {
     List<String> getPermissions(Long id);
--- dubbo-provider/src/java/com/jef/dubbo/demo/impl/DemoServiceImpl.java
+++ dubbo-provider/src/java/com/jef/dubbo/demo/impl/DemoServiceImpl.java
@@ -1,11 +1,16 @@
 package com.jef.dubbo.demo.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.jef.dubbo.demo.DemoService;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
+@Component
 public class DemoServiceImpl implements DemoService {
+    @Override
     public List<String> getPermissions(Long id) {
         List<String> demo = new ArrayList<String>();
         demo.add(String.format("Permission_%d", id - 1));

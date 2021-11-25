449ad205a23b8264e6e34cc0ff04680d821fa90a
jef.tu@foxmail.com <loveblog@520>
2021-11-25 12:30:55 +0800

    补充jdk8

--- java-jdk/src/test/java/com/jef/jdk8/FunctionTest.java
+++ java-jdk/src/test/java/com/jef/jdk8/FunctionTest.java
@@ -2,10 +2,15 @@ package com.jef.jdk8;
 
 import com.jef.constant.BasicConstant;
 import com.jef.constant.BasicEntity;
+import com.jef.constant.BasicList;
 import com.jef.entity.User;
 import com.jef.util.StringUtils;
+
+import com.google.common.collect.Maps;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.function.Function;
 
 /**
@@ -31,5 +36,12 @@ public class FunctionTest {
         Function<String, Integer> f2 = String::length;
         Integer length = f2.apply(BasicConstant.HELLO_WORLD);
         System.out.println("字符串长度=" + length);
+
+        // 同groupBy分组功能一样和toMap功能类似
+        List<User> userList = BasicList.getUserList();
+        Function<User, String> functionToMap = t -> t.getName() + t.getPhone();
+        HashMap<String, User> namePhoneMap = Maps.newHashMap();
+        userList.forEach(t -> namePhoneMap.put(functionToMap.apply(t), t));
+        System.out.println("namePhoneMap Size=" + namePhoneMap.size());
     }
 }

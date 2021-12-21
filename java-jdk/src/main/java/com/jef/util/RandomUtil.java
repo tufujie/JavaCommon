f088a666c52ca4fe5dde6ea19316aa792af7f68e
jef.tu@foxmail.com <loveblog@520>
2021-12-21 21:03:02 +0800

    ArrayIndex

--- java-jdk/src/main/java/com/jef/util/RandomUtil.java
+++ java-jdk/src/main/java/com/jef/util/RandomUtil.java
@@ -14,11 +14,11 @@ public class RandomUtil  extends Random {
     /**
      * 所有大小写字母和数字
      */
-    public static final String allChar = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String ALL_CHAR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     /**
      * 所有大小写字母
      */
-    public static final String letterChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LETTER_CHAR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     /**
      * 所有数字
      */
@@ -49,7 +49,7 @@ public class RandomUtil  extends Random {
         StringBuffer sb = new StringBuffer();
         Random random = new Random();
         for (int i = 0; i < length; i++) {
-            sb.append(allChar.charAt(random.nextInt(allChar.length())));
+            sb.append(ALL_CHAR.charAt(random.nextInt(ALL_CHAR.length())));
         }
         return sb.toString();
     }
@@ -64,7 +64,7 @@ public class RandomUtil  extends Random {
         StringBuffer sb = new StringBuffer();
         Random random = new Random();
         for (int i = 0; i < length; i++) {
-            sb.append(letterChar.charAt(random.nextInt(letterChar.length())));
+            sb.append(LETTER_CHAR.charAt(random.nextInt(LETTER_CHAR.length())));
         }
         return sb.toString();
     }

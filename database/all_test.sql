e9d7cf40a44b1f394473bc2538a00e58809c760f
jef.tu@foxmail.com <jef.tu@foxmail.com>
2022-02-24 12:24:45 +0800

    补充

--- database/all_test.sql
+++ database/all_test.sql
@@ -282,3 +282,30 @@ PRIMARY KEY (`FID`)
 -- shopID为2的使用order_info_1
 INSERT INTO all_test.t_bdc_splittablerule (FID,FTableName,FIsShopID,FCreateTime,FShopID,FSName,FShopName,FActualTable) VALUES
     ('1','order_info',1,'2021-04-14 11:54:34',2,'1','测试店铺01','order_info_1');
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for payment
+-- ----------------------------
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE `payment` (
+`id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+`serial` varchar(255) DEFAULT '' COMMENT '流水号',
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of payment
+-- ----------------------------
+BEGIN;
+INSERT INTO `payment` VALUES (1, 'No.1');
+INSERT INTO `payment` VALUES (2, 'No.2');
+INSERT INTO `payment` VALUES (3, 'No.3');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+

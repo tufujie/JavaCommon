063c0708d7ffb164ce8a75ca7a56945033dfda66
jef.tu@foxmail.com <loveblog@520>
2022-04-28 20:52:30 +0800

    如同反射般，却比反射强

--- java-jdk/src/test/java/com/jef/jdk8/FunctionTest.java
+++ java-jdk/src/test/java/com/jef/jdk8/FunctionTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 /**
@@ -43,5 +44,19 @@ public class FunctionTest {
         HashMap<String, User> namePhoneMap = Maps.newHashMap();
         userList.forEach(t -> namePhoneMap.put(functionToMap.apply(t), t));
         System.out.println("namePhoneMap Size=" + namePhoneMap.size());
+    }
+
+    @Test
+    public void testApplyAndAccept() {
+        Function<User, String> function = User::getName;
+        User user = BasicEntity.getUser();
+        String name = function.apply(user);
+        // 就是获取了对应属性的值
+        System.out.println("通过Function获取了属性：name=" + name);
+        name = name + "append";
+        // 然后设置对应属性的值
+        BiConsumer<User, String> consumer = User::setName;
+        consumer.accept(user, name);
+        System.out.println("通过Consumer设置了属性：name=" + user.getName());
     }
 }

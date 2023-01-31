68e43d3c38fb00d6cd07753d91d7a17f8366aa47
jef.tu@foxmail.com <jef.tu@foxmail.com>
2023-01-31 21:20:18 +0800

    补充算法

---
+++ java-component/src/main/java/com/jef/entity/ListNode.java
@@ -0,0 +1,22 @@
+package com.jef.entity;
+
+/**
+ * @author Jef
+ * @date 2023/1/31
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}

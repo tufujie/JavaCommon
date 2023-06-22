3f3d8aeba4cd3bf28e42d4fb125fff759700772c
jef.tu@foxmail.com <jef.tu@foxmail.com>
2023-06-22 08:11:52 +0800

    补充queue和stack

--- java-jdk/src/test/java/com/jef/algorithm/stackqueue/StackAndQueueTest.java
+++ java-jdk/src/test/java/com/jef/algorithm/stackqueue/StackAndQueueTest.java
@@ -1,8 +1,11 @@
 package com.jef.algorithm.stackqueue;
 
+import com.jef.util.Stack;
 import com.jef.util.algorithm.stackQueue.StackAndQueue;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * @author Jef
@@ -25,4 +28,33 @@ public class StackAndQueueTest {
         System.out.println(StackAndQueue.isPopOrder(arr1, new int[]{4, 5, 3, 2, 1}));
         System.out.println(StackAndQueue.isPopOrder(arr1, new int[]{4, 3, 5, 1, 2}));
     }
+
+    @Test
+    public void testQueue() {
+        ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<String>();
+        queue.add("1");
+        queue.add("2");
+        queue.add("3");
+        // 获取队首：1
+        System.out.println("peek()=" + queue.peek());
+        // 获取队首：1，并移除
+        System.out.println("poll()=" + queue.poll());
+        // 重新获取队首：2
+        System.out.println("peek()=" + queue.peek());
+    }
+
+    @Test
+    public void testStack() {
+        Stack<String> stack = new Stack<String>();
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        // 获取栈顶：3
+        System.out.println("peek()=" + stack.peek());
+        // 获取栈顶：3，并移除
+        System.out.println("pop()=" + stack.pop());
+        // 重新获取栈顶：2
+        System.out.println("peek()=" + stack.peek());
+    }
+
 }

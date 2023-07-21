67df446dbb2bc0594f57b3fdbcdd8bd16398daea
涂富杰 <tufujie@xgd.com>
2023-07-21 14:02:23 +0800

    补充单元测试-mockito测试

--- spring-boot/src/test/java/com/jef/jmockdata/MockitoTest.java
+++ spring-boot/src/test/java/com/jef/jmockdata/MockitoTest.java
@@ -2,7 +2,6 @@ package com.jef.jmockdata;
 
 import com.jef.dao.IUserDao;
 import com.jef.entity.User;
-import lombok.val;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,8 +34,10 @@ import static org.mockito.Mockito.*;
 public class MockitoTest {
 
     private static final String MOCKITO = "mockito";
+
     @Mock
     private IUserDao userDao;
+
     @Test
     @DisplayName("mockito api 测试")
     void testMockitoApi() {
@@ -95,6 +96,9 @@ public class MockitoTest {
     /**
      * 使用 spy() 部分模拟对象
      * Mockito 提供的 spy 方法可以包装一个真实的 Java 对象, 并返回一个包装后的新对象. 若没有特别配置的话, 对这个新对象的所有方法调用, 都会委派给实际的 Java 对象
+     * 这个例子中我们实例化了一个 LinkedList 对象, 然后使用 spy() 方法对 list 对象进行部分模拟. 接着我们使用 when(…).thenReturn(…) 方法链来规定 spy.size() 方法返回值是 100. 随后我们给 spy 添加了两个元素, 然后再 调用 spy.get(0) 获取第一个元素.
+     * 这里有意思的地方是: 因为我们没有定制 add(“one”), add(“two”), get(0), get(1), 因此通过 spy 调用这些方法时, 实际上是委派给 list 对象来调用的。
+     * 然而我们 定义了 spy.size() 的返回值, 因此当调用 spy.size() 时, 返回 100
      */
     @Test
     @DisplayName("spy api 测试")

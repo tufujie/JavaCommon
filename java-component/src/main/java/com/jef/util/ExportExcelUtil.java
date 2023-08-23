794c208a52b2d16de8970427d9185cc0bd17891c
涂富杰 <tufujie@xgd.com>
2023-08-23 15:25:21 +0800

    列表数据转excel

--- java-component/src/main/java/com/jef/util/ExportExcelUtil.java
+++ java-component/src/main/java/com/jef/util/ExportExcelUtil.java
@@ -3,9 +3,22 @@ package com.jef.util;
 
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.xssf.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFRichTextString;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -18,6 +31,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -49,7 +63,7 @@ public class ExportExcelUtil<T> {
         if(StringUtils.isEmpty(version) || EXCEL_FILE_2003.equals(version.trim())){
             exportExcel2003(title, null, dataset, out, "yyyy-MM-dd HH:mm:ss");
         }else{
-            exportExcel2007(title, null, dataset, out, "yyyy-MM-dd HH:mm:ss");
+            exportExcel2007(title, null, null, dataset, out, "yyyy-MM-dd HH:mm:ss");
         }
     }
 
@@ -69,7 +83,7 @@ public class ExportExcelUtil<T> {
         if(StringUtils.isBlank(version) || EXCEL_FILE_2003.equals(version.trim())){
             exportExcel2003(title, headers, dataset, out, "yyyy-MM-dd HH:mm:ss");
         }else{
-            exportExcel2007(title, headers, dataset, out, "yyyy-MM-dd HH:mm:ss");
+            exportExcel2007(title, headers, null, dataset, out, "yyyy-MM-dd HH:mm:ss");
         }
     }
 
@@ -92,7 +106,7 @@ public class ExportExcelUtil<T> {
      *            如果有时间数据，设定输出格式。默认为"yyyy-MM-dd hh:mm:ss"
      */
     @SuppressWarnings({ "unchecked", "rawtypes" })
-    public void exportExcel2007(String title, String[] headers, Collection<T> dataset, OutputStream out, String pattern) {
+    public void exportExcel2007(String title, String[] headers, String[] fields, Collection<T> dataset, OutputStream out, String pattern) {
         // 声明一个工作薄
         XSSFWorkbook workbook = new XSSFWorkbook();
         // 生成一个表格
@@ -146,7 +160,6 @@ public class ExportExcelUtil<T> {
         Iterator<T> it = dataset.iterator();
         int index = 0;
         T t;
-        Field[] fields;
         Field field;
         XSSFRichTextString richString;
         Pattern p = Pattern.compile("^//d+(//.//d+)?$");
@@ -164,12 +177,13 @@ public class ExportExcelUtil<T> {
             row = sheet.createRow(index);
             t = (T) it.next();
             // 利用反射，根据JavaBean属性的先后顺序，动态调用getXxx()方法得到属性值
-            fields = t.getClass().getDeclaredFields();
             for (int i = 0; i < fields.length; i++) {
                 cell = row.createCell(i);
                 cell.setCellStyle(style2);
-                field = fields[i];
-                fieldName = field.getName();
+                fieldName = fields[i];
+                if ("serialVersionUID".equals(fieldName)) {
+                    continue;
+                }
                 getMethodName = "get" + fieldName.substring(0, 1).toUpperCase()
                         + fieldName.substring(1);
                 try {
@@ -446,6 +460,134 @@ public class ExportExcelUtil<T> {
             cell.setCellStyle(cellStyle);
         }
         return cell;
+    }
+
+    public void exportExcel2007WithMap(String title, String[] headers, String[] fields, List<Map<String, Object>> dataset, OutputStream out, String pattern, Map<String, String> mapParams) {
+        // 声明一个工作薄
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        // 生成一个表格
+        XSSFSheet sheet = workbook.createSheet(title);
+        // 设置表格默认列宽度为15个字节
+        sheet.setDefaultColumnWidth(20);
+        // 生成一个样式
+        XSSFCellStyle style = workbook.createCellStyle();
+        // 设置这些样式
+        style.setFillForegroundColor(new XSSFColor(java.awt.Color.gray));
+        style.setFillPattern(XSSFCellStyle.SOLID_FOREGROUND);
+        style.setBorderBottom(XSSFCellStyle.BORDER_THIN);
+        style.setBorderLeft(XSSFCellStyle.BORDER_THIN);
+        style.setBorderRight(XSSFCellStyle.BORDER_THIN);
+        style.setBorderTop(XSSFCellStyle.BORDER_THIN);
+        style.setAlignment(XSSFCellStyle.ALIGN_CENTER);
+        // 生成一个字体
+        XSSFFont font = workbook.createFont();
+        font.setBoldweight(XSSFFont.BOLDWEIGHT_BOLD);
+        font.setFontName("宋体");
+        font.setColor(new XSSFColor(java.awt.Color.BLACK));
+        font.setFontHeightInPoints((short) 11);
+        // 把字体应用到当前的样式
+        style.setFont(font);
+        // 生成并设置另一个样式
+        XSSFCellStyle style2 = workbook.createCellStyle();
+        style2.setFillForegroundColor(new XSSFColor(java.awt.Color.WHITE));
+        style2.setFillPattern(XSSFCellStyle.SOLID_FOREGROUND);
+        style2.setBorderBottom(XSSFCellStyle.BORDER_THIN);
+        style2.setBorderLeft(XSSFCellStyle.BORDER_THIN);
+        style2.setBorderRight(XSSFCellStyle.BORDER_THIN);
+        style2.setBorderTop(XSSFCellStyle.BORDER_THIN);
+        style2.setAlignment(XSSFCellStyle.ALIGN_CENTER);
+        style2.setVerticalAlignment(XSSFCellStyle.VERTICAL_CENTER);
+        // 生成另一个字体
+        XSSFFont font2 = workbook.createFont();
+        font2.setBoldweight(XSSFFont.BOLDWEIGHT_NORMAL);
+        // 把字体应用到当前的样式
+        style2.setFont(font2);
+
+        // 产生表格标题行
+        XSSFRow row = sheet.createRow(0);
+        XSSFCell cellHeader;
+        for (int i = 0; i < headers.length; i++) {
+            cellHeader = row.createCell(i);
+            cellHeader.setCellStyle(style);
+            cellHeader.setCellValue(new XSSFRichTextString(headers[i]));
+        }
+
+        // 遍历集合数据，产生数据行
+        Iterator<Map<String, Object>> it = dataset.iterator();
+        int index = 0;
+        T t;
+        XSSFRichTextString richString;
+        Pattern p = Pattern.compile("^//d+(//.//d+)?$");
+        Matcher matcher;
+        String fieldName;
+        XSSFCell cell;
+        Object value;
+        String textValue;
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        while (it.hasNext()) {
+            index++;
+            row = sheet.createRow(index);
+            t = (T) it.next();
+            // 利用反射，根据JavaBean属性的先后顺序，动态调用getXxx()方法得到属性值
+            for (int i = 0; i < fields.length; i++) {
+                cell = row.createCell(i);
+                cell.setCellStyle(style2);
+                fieldName = fields[i];
+                try {
+                    value = ((Map<?, ?>) t).get(fieldName);
+                    // 判断值的类型后进行强制类型转换
+                    textValue = null;
+                    if (value instanceof Integer) {
+                        cell.setCellValue((Integer) value);
+                    } else if (value instanceof Float) {
+                        textValue = String.valueOf((Float) value);
+                        cell.setCellValue(textValue);
+                    } else if (value instanceof Double) {
+                        textValue = String.valueOf((Double) value);
+                        cell.setCellValue(textValue);
+                    } else if (value instanceof Long) {
+                        cell.setCellValue((Long) value);
+                    }
+                    if (value instanceof Boolean) {
+                        textValue = "是";
+                        if (!(Boolean) value) {
+                            textValue = "否";
+                        }
+                    } else if (value instanceof Date) {
+                        textValue = sdf.format((Date) value);
+                    } else {
+                        // 其它数据类型都当作字符串简单处理
+                        if (value != null) {
+                            textValue = value.toString();
+                        }
+                    }
+                    if (mapParams.containsKey(fieldName + (textValue != null ? textValue : ""))) {
+                        textValue = mapParams.get(fieldName + textValue);
+                    }
+                    if (textValue != null) {
+                        matcher = p.matcher(textValue);
+                        if (matcher.matches()) {
+                            // 是数字当作double处理
+                            cell.setCellValue(Double.parseDouble(textValue));
+                        } else {
+                            richString = new XSSFRichTextString(textValue);
+                            cell.setCellValue(richString);
+                        }
+                    }
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                } finally {
+                    // 清理资源
+                }
+            }
+        }
+        try {
+            workbook.write(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
